d9bef5affa7c2225157112885c2081b4dede535d
raffiabyann <raffizizou4@gmail.com>
2025-10-29 10:33:27 +0700

    Payment Class

--- src/week08/raffi/id/umn/ac/Payment.java
+++ src/week08/raffi/id/umn/ac/Payment.java
@@ -1,14 +1,45 @@
 package week08.raffi.id.umn.ac;
 
 public abstract class Payment {
-	protected double amount;
-	
-	public Payment (double amount) {
-		this.amount=amount;
-	}
-	abstract void processPayment ();
-	
-	public void paymentDetails() {
-		System.out.println("Processing payment of $" +  amount);
-	}
+    protected boolean isPaidOff;
+    protected Item item;
+
+    public abstract int pay();
+
+    public Payment() {
+        this.isPaidOff = false;
+        this.item = null;
+    }
+
+    public Payment(Item item) {
+        this.isPaidOff = false;
+        this.item = item;
+    }
+
+    public boolean isPaidOff() {
+        return isPaidOff;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public String getItemName() {
+        return item.getName();
+    }
+
+    public String getStatus() {
+        if (isPaidOff) {
+            return "FINISHED";
+        }
+        return "ON PROGRESS";
+    }
+
+    public int getRemainingAmount() {
+        if (isPaidOff) {
+            return 0;
+        }
+        return item.getPrice();
+    }
 }
+
